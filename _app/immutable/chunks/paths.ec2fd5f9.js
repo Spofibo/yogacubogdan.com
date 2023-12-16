6fd3ed47475d12e462eebc5836e9f70ce7bc72cb
Alex Budurovici <673081+w0rldart@users.noreply.github.com>
2023-12-16 12:04:28 +0100

    Updates

---
+++ _app/immutable/chunks/paths.ec2fd5f9.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_16a01ue)==null?void 0:s.base)??"/yogacubogdan.com";var a;const t=((a=globalThis.__sveltekit_16a01ue)==null?void 0:a.assets)??e;export{t as a,e as b};
