d28c5aaa64f3d8ecfac2aa4194d9d7d4ff001fda
Alex Budurovici <673081+w0rldart@users.noreply.github.com>
2023-12-16 11:47:26 +0100

    Install and configure svelte adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,34 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/kit/vite'
+import path from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess({})],
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: [vitePreprocess({})],
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      fallback: '404.html',
+      pages: 'build',
+      assets: 'build',
+      precompress: false,
+      strict: true
+    }),
+
+    alias: {
+      $assets: path.resolve('./src/lib/assets'),
+      $components: path.resolve('./src/lib/components'),
+      $images: path.resolve('./src/lib/assets/images'),
+      $lib: path.resolve('./src/lib'),
+    },
+
+    paths: {
+      // base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+      base: process.argv.includes('dev') ? '' : '/yogacubogdan.com'
+    }
+  }
 };
 
 export default config;
