137828910ee370f439a26c661aac50655968e530
Alex Budurovici <673081+w0rldart@users.noreply.github.com>
2024-02-18 15:19:35 +0100

    Update about page

--- src/routes/about/bogdan/+page.svelte
+++ src/routes/about/bogdan/+page.svelte
@@ -8,13 +8,9 @@
   <enhanced:img class="mask mask-squircle h-20" src={bogdan} alt="Bogdan" sizes="max(80px, 10vw)" />
 
   <p>
-    Rikard Mohlin underwent his Isha Hatha Yoga Teacher training in 2021 at the Isha Yoga Center, located in the foothills of Vellangiri
-    mountains in southern India. Brought up in south of Sweden he moved abroad after High School, having a simple and minimalistic way of
-    living and an approach to life that always was on the cross side. This led him to try out different forms of yoga and meditation before
-    coming to isha. "The moment I sat in my first Isha Yoga Class, I knew this is it, this is the way Yoga should be taught and practised."
+    Bogdan underwent his Isha Hatha Yoga Teacher training in 2021 at the Isha Yoga Center, located in the foothills of Vellangiri
+    mountains in southern India.
   </p>
-
-  <p>Rikard is now teaching regularly in Sweden and is conducting workshops in Stockholm and Malmö as well as online on a regular basis.</p>
 
   <p>
     Check out <a href="https://www.instagram.com/yogacubogdan/" class="link">"Yoga cu Bogdan" on Instagram</a>
